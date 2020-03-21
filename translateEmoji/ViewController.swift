317b6463cbb884978e40acb0cb930cffffb4cb5d
Lucas <wayne97023@gmail.com>
2020-03-21 12:46:51 +0800

    Initial Commit

---
+++ translateEmoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  translateEmoji
+//
+//  Created by 林奇杰 on 2020/3/21.
+//  Copyright © 2020 林奇杰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
